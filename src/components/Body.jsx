c3695ce463b94c5d47d66054600958cb66b25b27
rajato1209 <rajato1209@gmail.com>
2025-12-30 22:13:09 +0530

    added functionality to body page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,36 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import RestroCards from './RestroCards'
+import React, { useEffect, useState } from "react";
+import RestroCards from "./RestroCards";
 
 const Body = () => {
-  let [resList,setResList]=useState([]);
+  let [resList, setResList] = useState([]);
+  let [filterData, setFilterData] = useState([]);
+  let [search, setSearch] = useState("");
 
-  useEffect(()=>{
-    fetchData();    
-  },[])
+  const fetchData = async () => {
+    let data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.97210&lng=72.82460"
+    );
+    let res = await data.json();
+    console.log(res);
+    setResList(
+      res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setFilterData(
+      res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const fetchData=async()=>{
-    let data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.97210&lng=72.82460");
-    let res=await data.json();
-    // console.log("fetch data",res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
-   setResList(res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+  if (resList.length === 0) {
+    return <h1>Loading... </h1>;
   }
-   if(resList.length===0){
-    console.log("Loading...")
-  }
+
+  const handleSearch = () => {
+    let data = resList.filter((el) => el.info.name.toLowerCase().includes(search.toLowerCase()));
+    // console.log("search data:",data)
+    setFilterData(data)
+  };
+
   return (
     <>
-        <div className="filter">
-            <button style={{marginLeft:"16%"}}>Top Rated Restaurants</button>
+      <div className="Sfbutton">
+        <div>
+          <button style={{ marginLeft: "16%" }}>Top Rated Restaurants</button>
         </div>
-        <div className="ResCard">
-         { resList.map((el)=>(
-            <RestroCards key={el.info.id} resData={el}/>
-          ))}
-            {/* <RestroCards/> */}
-        
+        <div className="search-box">
+          <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)} />
+          <button onClick={handleSearch}>Search</button>
         </div>
-    </>
-  )
-}
+      </div>
 
-export default Body
+      {/* restro card */}
+      <div className="ResCard">
+        {filterData.map((el) => (
+          <RestroCards key={el.info.id} resData={el} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Body;
--- src/components/RestroCards.jsx
+++ src/components/RestroCards.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
 const RestroCards = (el) => {
-  console.log("element inside restro card",el)
+  // console.log("element inside restro card", el);
   return (
     <>
       <div className="card_Container">
         <div id="card">
           <div className="image">
-            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + el.resData.info.cloudinaryImageId} alt="" /> 
+            <img
+              src={
+                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+                el.resData.info.cloudinaryImageId
+              }
+              alt=""
+            />
           </div>
           <div className="resDetails">
             <h3>{el.resData.info.name}</h3>
+            <p>{el.resData.info.cuisines.join(" , ")}</p>
+
             <h4>{el.resData.info.avgRating}</h4>
-            {/* <p>{el.resData.info.  }</p> */}
           </div>
         </div>
       </div>
